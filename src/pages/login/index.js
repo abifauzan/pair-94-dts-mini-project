59f0df85fde66fcbc6be2cb3c1ef60569b82fb7e
barkahharis <barkahris@gmail.com>
2022-07-21 09:52:42 +0700

    add login reg ui

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
+
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../configs/firebase";
 import useAuth from "../../hooks/useAuth";
+import logo from "../../assets/logo.svg";
 
 const defaultValue = {
   email: "",
@@ -38,45 +40,76 @@ const Login = () => {
       // setErrorMessage(error);
     }
   };
-
+  const externalImage = "https://rb.gy/p2hphi";
   return (
-    <form onSubmit={handleSubmit}>
-      <h2> Login Page </h2>
-      <div>
-        <p>Email:</p>
-        <input
-          onChange={(event) => handleInputChange(event.target.value, "email")}
-          type="email"
-          name="email"
-          placeholder="Enter email..."
-        />
-      </div>
-      <div>
-        <p>Password:</p>
-        <input
-          onChange={(event) => handleInputChange(event.target.value, "password")}
-          type="password"
-          name="password"
-          placeholder="Enter password..."
-        />
-      </div>
-      <div>
-        <hr />
-        <button type="submit">Login</button>
-        <button
-          type="button"
-          onClick={() => {
-            navigate("/register", { state });
-            // navigate("/register");
-          }}
-        >
-          Register
-        </button>
-        <hr />
-      </div>
-      <div>{errorMessage}</div>
-    </form>
-  );
+    
+    <div 
+      className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent"
+      style={{
+        backgroundImage: `url(${externalImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        layout: 'fill',
+        objectFit: 'cover',
+        
+      }}
+    >
+        {/* <img
+          src="https://rb.gy/p2hphi"
+          layout="fill"
+          className="-z-10 !hidden opacity-60 sm:!inline"
+          objectFit="cover"
+        /> */}
+        
+        <img
+          src={logo}
+          className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+          width={100}
+          height={100}
+        /> 
+
+        <form onSubmit={handleSubmit}
+          className="relative mt-24 space-y-8 rounded bg-black/60 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h1 className="text-4xl text-white font-semibold">Sign In</h1>
+          <div className="space-y-4">
+            <label className="inline-block w-full">
+            <input 
+              onChange={(event) => handleInputChange(event.target.value, "email")} 
+              className={
+                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              } 
+              type="email" name="email" placeholder="Enter email..." />
+            </label>
+            <label className="inline-block w-full">
+            <input onChange={(event) => handleInputChange(event.target.value, "password")} 
+              className={
+                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              } 
+              type="password" name="password" placeholder="Enter password..." />
+            </label>
+          </div>
+
+        <button className="w-full rounded bg-[#E50914] py-3 font-semibold text-white">Sign In</button>
+
+        <div className="text-[gray]">
+          New to Movies?{'  '}
+          <button
+            type="submit"
+            className="cursor-pointer text-white hover:underline"
+            onClick={() => {
+              navigate("/register", { state });
+              // navigate("/register");
+            }}>
+            Sign up now
+          </button>
+        </div>
+            
+
+        </form>
+    </div>
+  )
+
 };
 
 export default Login;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../configs/firebase";
+import logo from "../../assets/logo.svg";
 
 const defaultValue = {
   email: "",
@@ -40,7 +41,7 @@ const Register = () => {
     }
   };
 
-  return (
+  /* return (
     <form onSubmit={handleSubmit}>
       <h2> Register Page </h2>
       <div>
@@ -77,7 +78,68 @@ const Register = () => {
       </div>
       <div>{errorMessage}</div>
     </form>
+  ); */
+  const externalImage = "https://rb.gy/p2hphi";
+  return (
+  <div 
+    className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent"
+    style={{
+      backgroundImage: `url(${externalImage})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      layout: 'fill',
+      objectFit: 'cover',
+    }}
+  >
+        
+        
+        <img
+          src={logo}
+          className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+          width={100}
+          height={100}
+        /> 
+
+        <form onSubmit={handleSubmit}
+          className="relative mt-24 space-y-8 rounded bg-black/60 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h1 className="text-4xl text-white font-semibold">Sign Up Now</h1>
+          <div className="space-y-4">
+            <label className="inline-block w-full">
+            <input 
+              onChange={(event) => handleInputChange(event.target.value, "email")} 
+              className={
+                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              } 
+              type="email" name="email" placeholder="Enter email..." />
+            </label>
+            <label className="inline-block w-full">
+            <input onChange={(event) => handleInputChange(event.target.value, "password")} 
+              className={
+                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
+              } 
+              type="password" name="password" placeholder="Enter password..." />
+            </label>
+          </div>
+
+        <button className="w-full rounded bg-[#E50914] py-3 font-semibold text-white">Sign Up</button>
+
+        <div className="text-[gray]">
+          Already have account?{'  '}
+          <button
+            type="submit"
+            className="cursor-pointer text-white hover:underline"
+            onClick={() => {
+              navigate("/login", { state });
+              // navigate("/register");
+            }}>
+            Sign In Now
+          </button>
+        </div>
+        </form>
+    </div>
   );
+
 };
 
 export default Register;
